fe2e7fac96fd6c3da8439f6eca5ef92ae980890c
James Ramirez <jramire678@gmail.com>
2020-04-21 01:59:50 -0400

    Updates

---
+++ precache-manifest.d153fb45b22c4102e1c93970ab5189fa.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edb3085919c0882a8de87d2becedd9c0",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "9c127df22702cec70a44",
+    "url": "/portfolio/static/css/main.ad9142c1.chunk.css"
+  },
+  {
+    "revision": "f8fc0bcd5443e6d61597",
+    "url": "/portfolio/static/js/2.0ee846b0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.0ee846b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c127df22702cec70a44",
+    "url": "/portfolio/static/js/main.aadb23e3.chunk.js"
+  },
+  {
+    "revision": "6c1d3ad26a766dc494c1",
+    "url": "/portfolio/static/js/runtime-main.d597d31e.js"
+  },
+  {
+    "revision": "35bf67485477067466fc1bd6bb4f72ce",
+    "url": "/portfolio/static/media/Art1.35bf6748.png"
+  },
+  {
+    "revision": "fdeb1f9c9e8b85587fdc3fc28c3ccc6b",
+    "url": "/portfolio/static/media/Art2.fdeb1f9c.png"
+  },
+  {
+    "revision": "688946fdb1ab6141ea8affd0f0a59891",
+    "url": "/portfolio/static/media/Art3.688946fd.png"
+  },
+  {
+    "revision": "330713139b1a9ec3c3cebbc57ced8a9d",
+    "url": "/portfolio/static/media/Profile.33071313.png"
+  },
+  {
+    "revision": "369a94198c4ae7820513f19babb30d86",
+    "url": "/portfolio/static/media/Study.369a9419.png"
+  },
+  {
+    "revision": "8651ea46398c5a25c88aebaf7c3c9319",
+    "url": "/portfolio/static/media/Study2.8651ea46.png"
+  },
+  {
+    "revision": "be9d84534f4f3f0f719694739dfe441d",
+    "url": "/portfolio/static/media/Study3.be9d8453.png"
+  },
+  {
+    "revision": "03be077da413a7a3f7c658a45c34a5e0",
+    "url": "/portfolio/static/media/Vid1.03be077d.mp4"
+  },
+  {
+    "revision": "bea3feed34d4a9dd166ab375bb33865d",
+    "url": "/portfolio/static/media/Vid2.bea3feed.mp4"
+  }
+]);
